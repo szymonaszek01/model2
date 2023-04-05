7f54e35ed17bcdbb20cba5f2af4f13c390029e7a
szymonaszek01 <235879@edu.p.lodz.pl>
2023-04-05 14:37:29 +0200

    task-004 Adding new queries in repository package

--- src/main/java/com/oro/model2/repository/ScreeningRepository.java
+++ src/main/java/com/oro/model2/repository/ScreeningRepository.java
@@ -1,9 +1,28 @@
 package com.oro.model2.repository;
 
+import com.oro.model2.entity.Film;
 import com.oro.model2.entity.Screening;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ScreeningRepository extends JpaRepository<Screening, Long> {
+
+    @Query("SELECT DISTINCT s.film FROM Screening s WHERE s.auditorium.id = :auditoriumId")
+    List<Film> findAllFilmsByAuditoriumId(long auditoriumId);
+
+    @Query("SELECT s FROM Screening s WHERE s.auditorium.id = :auditoriumId")
+    List<Screening> findAllScreeningsByAuditoriumId(long auditoriumId);
+
+    @Query("SELECT s FROM Screening s WHERE s.film.id = :filmId")
+    List<Screening> findAllScreeningsByFilmId(long filmId);
+
+    @Query("SELECT s FROM Screening s WHERE s.film.name = :name")
+    List<Screening> findAllScreeningsByFilmName(String name);
+
+    @Query("SELECT COUNT(s.auditorium) FROM Screening s WHERE s.film.id = :filmId")
+    long countAllAuditoriumsByFilmId(long filmId);
 }
--- src/main/java/com/oro/model2/repository/TicketRepository.java
+++ src/main/java/com/oro/model2/repository/TicketRepository.java
@@ -1,9 +1,30 @@
 package com.oro.model2.repository;
 
+import com.oro.model2.entity.Screening;
 import com.oro.model2.entity.Ticket;
+import com.oro.model2.entity.UserProfile;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Repository
 public interface TicketRepository extends JpaRepository<Ticket, Long> {
+
+    @Query("SELECT DISTINCT t.reservation.userProfile FROM Ticket t WHERE t.screening.id = :id")
+    List<UserProfile> findAllUserProfilesByScreeningId(long id);
+
+    @Query("SELECT t.screening FROM Ticket t WHERE t.reservation.userProfile.id = :id")
+    List<Screening> findAllScreeningsByUserProfileId(long id);
+
+    @Query("SELECT t.screening FROM Ticket t WHERE t.reservation.userProfile.email = :email")
+    List<Screening> findAllScreeningsByUserProfileEmail(String email);
+
+    @Query("SELECT COUNT(t.seat) FROM Ticket t WHERE t.screening.auditorium.id = :auditoriumId AND t.screening.id = :screeningId")
+    long countAllSeatsByAuditoriumIdAndScreeningId(long auditoriumId, long screeningId);
+
+    @Query("SELECT COUNT(t) FROM Ticket t WHERE t.reservation.userProfile.id = :userProfileId AND t.screening.screeningStart >= :screeningStart AND t.screening.screeningEnd <= :screeningEnd")
+    long countAllTicketsByUserProfileIdAndScreeningStartAndScreeningEnd(long userProfileId, LocalDateTime screeningStart, LocalDateTime screeningEnd);
 }
